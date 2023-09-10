44cabaa588d283c34f1cfdeccebae2febb9666dc
Isaac <nolimitisaakios@gmail.com>
2023-09-11 03:30:45 +0800

    Removed unnecessary files

--- testing_2.py
+++
@@ -1,8 +0,0 @@
-def areaOfCircle():
-    return math.pi * radius * radius
-
-def volumeOfCircle():
-    return length * math.pi * radius * radius
-
-def volumeOfCone():
-    return 1/3 * height * math.pi * radius * radius
