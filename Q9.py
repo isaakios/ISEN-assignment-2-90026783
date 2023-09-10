a35186b2012448095442160636f566313378c596
Isaac <nolimitisaakios@gmail.com>
2023-09-11 01:58:14 +0800

    question 9 done

--- Q9.py
+++ Q9.py
@@ -24,6 +24,62 @@ def move_direction(direction, x, y):
         y += 1
     else:
         raise ValueError('Invalid direction')
-    print(x, y)
+    return x, y
     
 #your code below this line ----------------------------
+import unittest
+
+
+class TestMoveDirection(unittest.TestCase):
+
+    def test_invalid_position(self):
+        with self.assertRaises(ValueError) as context:
+            move_direction("N", 8, 5)
+        self.assertEqual(str(context.exception), "Invalid position")
+
+        with self.assertRaises(ValueError) as context:
+            move_direction("S", 6, 8)
+        self.assertEqual(str(context.exception), "Invalid position")
+
+        with self.assertRaises(ValueError) as context:
+            move_direction("N", -1, 5)
+        self.assertEqual(str(context.exception), "Invalid position")
+
+        with self.assertRaises(ValueError) as context:
+            move_direction("N", 6, -1)
+        self.assertEqual(str(context.exception), "Invalid position")
+
+    def test_direction(self):
+
+        # Test North ('N')
+        self.assertEqual(move_direction("N", 2, 5) , (2, 4)), "Failed for 'N'"
+
+        # Test South ('S')
+        self.assertEqual(move_direction("S", 2, 5) , (2, 6)), "Failed for 'S'"
+
+        # Test East ('E')
+        self.assertEqual(move_direction("E", 2, 5) , (3, 5)), "Failed for 'E'"
+
+        # Test West ('W')
+        self.assertEqual(move_direction("W", 2, 5) , (1, 5)), "Failed for 'W'"
+
+        # Test North-East ('NE')
+        self.assertEqual(move_direction("NE", 2, 5) , (3, 4)), "Failed for 'NE'"
+
+        # Test North-West ('NW')
+        self.assertEqual(move_direction("NW", 2, 5) , (1, 4)), "Failed for 'NW'"
+
+        # Test South-East ('SE')
+        self.assertEqual(move_direction("SE", 2, 5) , (3, 6)), "Failed for 'SE'"
+
+        # Test South-West ('SW')
+        self.assertEqual(move_direction("SW", 2, 5) , (1, 6)), "Failed for 'SW'"
+
+
+    def test_invalid_direction(self):
+        with self.assertRaises(ValueError) as context:
+            move_direction("XYZ", 7, 5)
+        self.assertEqual(str(context.exception), "Invalid direction")
+
+if __name__ == "__main__":
+    unittest.main()
